a124c6d82f06a0e3c8de5fa716a37aef3a714d84
Chesneau Luc <luc.chesneau41@gmail.com>
2022-10-03 17:25:29 +0200

    User left the chat implémenté.

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ const io = new Server(server);
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import dbConnect from "./sequelize.js";
-import { getUsername } from './public/vues/connected.js'
+import { getUsername } from "./public/vues/connected.js";
+import cookie from "cookie";
+import jwt from "jsonwebtoken";
 
 dbConnect();
-
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -20,13 +21,31 @@ const rootDir = __dirname;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
-app.use("/public", express.static('public'));
-app.use("/models", express.static('models'));
+app.use("/public", express.static("public"));
+app.use("/models", express.static("models"));
+
 
 
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+
+    const cookies = cookie.parse(socket.handshake.headers.cookie);
+    const usernameToleft = jwt.verify(cookies.token, process.env.SECRET).username;
+    console.log(usernameToleft);
+
+  socket.on("connexion_user", (cookie) => {
+    const username = getUsername(cookie);
+    if (username) {
+      socket.broadcast.emit("connexion_user", `${username} has joined the chat`);
+      socket.emit("online_user", username);
+    } else {
+      socket.broadcast.emit(
+        "connexion_user",
+        `An anonyme user has joined the chat`
+      );
+    }
+  });
+
   socket.on("chat message", (msg, cookie) => {
     const username = getUsername(cookie);
     if (username) {
@@ -35,16 +54,17 @@ io.on("connection", (socket) => {
       io.emit("chat message", msg);
     }
   });
+
   socket.on("disconnect", () => {
     console.log("user disconnected");
+    socket.broadcast.emit("disconnection_message", usernameToleft)
   });
-  socket.broadcast.emit("hi");
+
 });
 
 app.listen(3001, () => {
   console.log("listening on *:3001");
 });
-
 
 server.listen(3000, () => {
   console.log("Server is running on port 3001");
--- public/vues/index.js
+++ public/vues/index.js
@@ -1,9 +1,12 @@
 const socket = io();
 const messages = document.getElementById('messages');
+const onlineUl = document.getElementById('users-ul');
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 
-// const username = getUsername();
+
+
+socket.emit('connexion_user', document.cookie);
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
@@ -11,12 +14,32 @@ form.addEventListener('submit', function(e) {
     socket.emit('chat message', input.value, document.cookie);
     input.value = '';
   }
-  
 });
 
 socket.on('chat message', function(msg) {
   const item = document.createElement('li');
   item.textContent = msg;
   messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+socket.on("connexion_user", function(msg) {
+  const item = document.createElement('li');
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+  
+socket.on("disconnection_message", function(msg) {
+  const item = document.createElement('li');
+  msg == null ? item.textContent = "An anonyme user has left the chat" : item.textContent = msg + " has left the chat";
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+socket.on("online_user", function(msg) {
+  const item = document.createElement('li');
+  item.textContent = msg + " is online";
+  onlineUl.appendChild(item);
   window.scrollTo(0, document.body.scrollHeight);
 });
